844b9fdee37195537af9c7e8f3324f43ee41f85b
Vinny Diehl <vinny.diehl@gmail.com>
2025-09-21 09:18:20 -0700

    Spawn multiple aircraft at a set interval

--- app/controller/constants.rb
+++ app/controller/constants.rb
@@ -1,7 +1,7 @@
 AIRCRAFT_SIZE = 32
 AIRCRAFT_RADIUS = AIRCRAFT_SIZE / 2
 # Pixels/second
-AIRCRAFT_SPEED = 50
+AIRCRAFT_SPEED = 25
 
 # How far off the screen an aircraft spawns
 SPAWN_PADDING = AIRCRAFT_SIZE * 3
--- app/controller/scenes/game.rb
+++ app/controller/scenes/game.rb
@@ -1,10 +1,18 @@
 class ControllerGame
   def game_init
-    @aircraft = [Aircraft.new]
+    @aircraft = []
+
+    # Seconds between aircraft spawns
+    @spawn_interval = 10
   end
 
   def game_tick
     handle_mouse_inputs
+
+    if @ticks % @spawn_interval.seconds == 0
+      @aircraft << Aircraft.new
+    end
+
     @aircraft.each(&:tick)
   end
 end
