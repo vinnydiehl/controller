64fc13ac5ae81366bfcddc156d317aece229e9a4
Vinny Diehl <vinny.diehl@gmail.com>
2025-10-05 00:39:13 -0700

    Ease `Aircraft` speed during takeoff

--- app/controller/aircraft.rb
+++ app/controller/aircraft.rb
@@ -33,11 +33,10 @@ class Aircraft
 
   def initialize(position:, type:, speed:, runway:, vtol:,
                  course: nil, departing: nil, size: AIRCRAFT_SIZE)
-    @position, @type, @speed, @runway_type, @vtol, @course, @departing, @size =
-      position, type, speed, runway, vtol, course, departing, size
+    @position, @type, @runway_type, @vtol, @course, @departing, @size =
+      position, type, runway, vtol, course, departing, size
 
-    # Pixels/frame
-    @speed_px = @speed / 60.0
+    set_speed(speed)
 
     # Array of "waypoints" which connect to form the path the
     # aircraft will follow
@@ -72,6 +71,9 @@ class Aircraft
       # onto the runway and becoming airborne. Once it's airborne,
       # this will be set to false.
       @taking_off = true
+      # The speed will also need to accelerate to the set speed, so save this
+      @cruise_speed = speed
+      set_speed(TAXI_SPEED)
     else
       # Otherwise we've spawned an incoming aircraft off-screen.
       # We need to figure out where on the screen the aircraft is going
@@ -136,8 +138,8 @@ class Aircraft
       move_along_heading
     end
 
-    # If the aircraft is taking off, ease the aircraft size as it
-    # becomes airborne
+    # If the aircraft is taking off, ease the aircraft's size and
+    # speed as it becomes airborne
     if taking_off? && path.size == 1
       @takeoff_run ||= Geometry.distance(@position, @path[0])
       run_remaining = Geometry.distance(@position, @path[0])
@@ -148,6 +150,7 @@ class Aircraft
       eased = 1 - (1 - progress)**2
 
       @size = DEPARTURE_SIZE + (AIRCRAFT_SIZE - DEPARTURE_SIZE) * eased
+      set_speed(TAXI_SPEED + (@cruise_speed - TAXI_SPEED) * eased)
     end
 
     if @emergency
@@ -157,6 +160,11 @@ class Aircraft
     handle_screen_edge_collision
     handle_departure
     ease_heading
+  end
+
+  def set_speed(speed)
+    @speed = speed
+    @speed_px = @speed / 60
   end
 
   # Make the aircraft smoothly pathfind towards +runway+ and land there.
